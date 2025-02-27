da57e61a856e9b7a9a96bef2a2cbbfeb98cef84b
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2025-02-27 20:32:46 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,13 @@
+--Create new database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+create schema bronze;
+
+create schema silver;
+
+create schema gold;
